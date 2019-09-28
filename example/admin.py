d88d06c6728ab5254113d14f9eeb09e2d59662b3
szponovic <tomasz.kamil.biskupski@gmail.com>
2019-09-28 11:42:22 +0200

    second upload

--- example/admin.py
+++ example/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from example.models import Movie, Genre
 
-# Register your models here.
+
+class MovieInLine(admin.TabularInline):
+    model = Movie
+    extra = 1
+
+
+class GenreListAdmin(admin.ModelAdmin):
+    inlines = (MovieInLine,)
+
+
+admin.site.register(Movie)
+admin.site.register(Genre, GenreListAdmin)
