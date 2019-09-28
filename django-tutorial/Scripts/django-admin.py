9de392ba999c5bc23866ba4aff74f363f8f551a3
szponovic <tomasz.kamil.biskupski@gmail.com>
2019-09-28 10:55:36 +0200

    'delete'

--- django-tutorial/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Tomasz Biskupski\Desktop\nauka python\django-tutorial\django-tutorial\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
